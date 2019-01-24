734758fa9a067355bad7f37f9a788e28d23aa43d
Benjamin Berglund <benjaminberglund@gmail.com>
2019-01-24 16:06:12 +0100

    Done tha form

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,6 +1,22 @@
 <template>
   <article>
     <h1>Register</h1>
+    <form action="/action_page.php">
+      <div class="form-group">
+        <label for="email">Email address:</label>
+        <input type="email" class="form-control" id="email">
+      </div>
+      <div class="form-group">
+        <label for="pwd">Password:</label>
+        <input type="password" class="form-control" id="pwd">
+      </div>
+      <div class="form-group form-check">
+        <label class="form-check-label">
+          <input class="form-check-input" type="checkbox"> Remember me
+        </label>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
   </article>
 </template>
 
